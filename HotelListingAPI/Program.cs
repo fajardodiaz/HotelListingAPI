5eb3d0eb1fad07e8ed4bacfdde380e99f90c6cae
Pablo Fajardo <pfajardodiaz17@gmail.com>
2024-06-06 11:17:24 -0500

    Adding Logging with Serilog

--- HotelListingAPI/Program.cs
+++ HotelListingAPI/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.Extensions.Options;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +14,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAll", b => b.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 });
 
+// Serilog Configuration
+builder.Host.UseSerilog((ctx, lc)=>lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
